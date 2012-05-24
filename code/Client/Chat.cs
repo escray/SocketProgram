e309459d9afe1c7a25e5bc9261efb928d06c5ffa
escray <escray@live.com>
2012-05-24 17:55:13 +0800

    客户端按钮屏蔽问题还没解决

--- code/Client/Chat.cs
+++ code/Client/Chat.cs
@@ -8,10 +8,6 @@ namespace Client
 {
     public partial class Chat : Form
     {
-        /// <summary>
-        /// 接收数据缓冲区大小 
-        /// </summary>
-        private int maxPacket = 1024;
 
         public Chat()
         {
@@ -23,8 +19,6 @@ namespace Client
         public ChatType ChatType { get; set; }
         public string ReceiveName { get; set; }
 
-        private Thread receiveThread;
-       
         public void DisplayMessage(string msg)
         {
             rtbMessageDisplay.AppendText(msg);
@@ -33,6 +27,13 @@ namespace Client
 
         private void btnQuit_Click(object sender, EventArgs e)
         {
+            var main = Parent as Main;
+            if (main != null)
+            {
+                main.ChatButton.Enabled = true;
+                main.BroadCastButton.Enabled = true;
+            }
+
             Close();
         }
 
@@ -43,7 +44,7 @@ namespace Client
             string sendmsg = string.Empty;
             if (string.IsNullOrEmpty(msg))
             {
-                MessageBox.Show("不能发送空消息", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                MessageBox.Show(@"不能发送空消息", @"提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 return;
             }
             
@@ -52,25 +53,37 @@ namespace Client
             {
                 localmsg = "[广播]您在 " + DateTime.Now + " 对所有人说\r\n" + msg + "\r\n\r\n";
                 sendmsg = "[广播]" + UserName + " 在 " + DateTime.Now + " 对所有人说：\r\n" + msg + "\r\n\r\n";
-                byte[] cmdmsg = Encoding.Unicode.GetBytes("cmd::BroadCast");
-                Stream.Write(cmdmsg, 0, cmdmsg.Length);
+
                 
+                SendMessage(Stream, "cmd::BroadCast");
             }
             else if(ChatType == ChatType.Private)
             {
                 sendmsg = string.Format("[{0}] : {1} {2}\r\n", UserName, msg, DateTime.Now.ToShortTimeString());
                 localmsg = sendmsg;
 
-                byte[] rcvName = Encoding.Unicode.GetBytes(ReceiveName);
-                Stream.Write(rcvName, 0, rcvName.Length);
+                SendMessage(Stream, ReceiveName);
             }
 
-            byte[] sendbytes = Encoding.Unicode.GetBytes(sendmsg);
-            Stream.Write(sendbytes, 0, sendbytes.Length);
-            
+            SendMessage(Stream, sendmsg);
 
-            rtbMessageDisplay.AppendText(localmsg);
+            DisplayMessage(localmsg);
             tbxSendMessage.Clear();
+        }
+
+        private void SendMessage(NetworkStream stream, string msg)
+        {
+            try
+            {
+                byte[] cmdmsg = Encoding.Unicode.GetBytes(msg);
+                stream.Write(cmdmsg, 0, cmdmsg.Length);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show(@"网络中断，无法连接到服务器，请重试！", @"错误", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+            
         }
 
         private void Chat_Load(object sender, EventArgs e)
